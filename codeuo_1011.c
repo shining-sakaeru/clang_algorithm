219a89856d0489daa59d4c2a324497e6fa8a7487
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-03 12:25:38 +0900

    first releas

---
+++ codeuo_1011.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char x;
+    scanf("%c",&x);
+    printf("%c",x);
+   
+   
+    return 0;
+    
+}
---
+++ codeuo_1012.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    float x;
+    scanf("%f",&x);
+    printf("%f",x);
+   
+   
+    return 0;
+    
+}
